969e407e027db52ba1eadf862676d98822f8fe45
Jerry Janvier <jerrythefilmmaker@gmail.com>
2024-02-23 11:23:03 -0500

    needs fixing

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+
+  const [selectedCategory, setSelectedCategory] = useState("All")
+
+  function handleChange(e) {
+    console.log(e.target.value)
+    setSelectedCategory(e.target.value)
+  }
+
+  const filteredItems = items.filter(
+    (item) => selectedCategory === "All" ? true : item.category === selectedCategory
+  )
+
+  console.log(filteredItems)
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+
+        <select onChange={handleChange} name="filter">
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +28,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {filteredItems.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
